58bf0edc0d9ff8390836d1c7018b8502f4635de7
Marc-Andre Bourassa <boumar04@gmail.com>
2020-07-24 17:43:01 -0400

    product component

--- src/components/productSection.js
+++ src/components/productSection.js
@@ -2,17 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import Img from "gatsby-image";
 
-function ProductSection({ title, text, photo, reverseOrder }) {
+function ProductSection({ title, paragraphes, photo, reverseOrder }) {
   console.log(photo);
   return (
     <section className="py-20">
       <div className="container mx-auto px-16 items-center flex flex-col md:flex-row">
-        <div className="md:w-1/2">
+        <div className="md:w-1/2 md:pr-32 lg:pr-48">
           <h3 className="text-3xl font-semibold leading-tight">{title}</h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">{text}</p>
+          {paragraphes.map((paragraphe) => (
+            <p key={paragraphe.id} className="mt-4 text-lg font-light">
+              {paragraphe}
+            </p>
+          ))}
         </div>
-        <div className={`mt-10 lg:mt-0 w-full lg:w-1/2 ${reverseOrder && `order-last md:order-first`}`}>
-          <Img alt={title} fixed={photo}></Img>
+        <div className={`mt-10 md:mt-0 w-full md:w-1/2 ${reverseOrder && `order-first md:order-last`}`}>
+          <Img alt={title} fluid={photo}></Img>
         </div>
       </div>
     </section>
@@ -23,7 +27,7 @@ export default ProductSection;
 
 ProductSection.propTypes = {
   title: PropTypes.string,
-  text: PropTypes.string,
+  paragraphes: PropTypes.arrayOf(PropTypes.string),
   photo: PropTypes.object,
   reverseOrder: PropTypes.bool,
 };
--- src/pages/services.js
+++ src/pages/services.js
@@ -19,11 +19,14 @@ export default function ServicesPage({ data }) {
       ></Header>
       <ProductSection
         title="Lattes de Vinyle"
-        text={
-          "Le revêtement de plancher en lattes de vinyle est un type innovateur de vinyle qui a l'allure du vrai bois et qui en donne la sensation."
-        }
+        paragraphes={[
+          "Le revêtement de plancher en lattes de vinyle est un type innovateur de vinyle qui a l'allure du vrai bois et qui en donne la sensation.",
+          "En outre, il est étanche, ce qui permet de l'installer dans des pièces propices à l'humidité, tel que les sous-sols et les salles d'eau, là où les bois franc et stratifiés sont à proscrire.",
+          "De plus, le revêtement de sol en planches de vinyle est silencieux, résistant et chaud sous les pieds. C'est aussi l'un des choix les plus abordables sur le marché.",
+          "Les lattes de vinyles tendent à dépasser en popularité le plancher de stratifié (plancher flottant).",
+        ]}
         reverseOrder={false}
-        photo={data.vinyle.childImageSharp.fixed}
+        photo={data.vinyle.childImageSharp.fluid}
       ></ProductSection>
     </Layout>
   );
@@ -40,50 +43,57 @@ export const query = graphql`
     }
     vinyle: file(relativePath: { eq: "products/vinyle.webp" }) {
       childImageSharp {
-        fixed(quality: 100) {
-          ...GatsbyImageSharpFixed
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
         }
       }
     }
     creamique: file(relativePath: { eq: "products/creamique.webp" }) {
       childImageSharp {
-        fixed(quality: 100) {
-          ...GatsbyImageSharpFixed
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
         }
       }
     }
     flottant: file(relativePath: { eq: "products/flottant.webp" }) {
       childImageSharp {
-        fixed(quality: 100) {
-          ...GatsbyImageSharpFixed
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
         }
       }
     }
     ingenerie: file(relativePath: { eq: "products/ingenerie.webp" }) {
       childImageSharp {
-        fixed(quality: 100) {
-          ...GatsbyImageSharpFixed
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
         }
       }
     }
     linoleum: file(relativePath: { eq: "products/linoleum.webp" }) {
       childImageSharp {
-        fixed(quality: 100) {
-          ...GatsbyImageSharpFixed
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
         }
       }
     }
     prelart: file(relativePath: { eq: "products/prelart.webp" }) {
       childImageSharp {
-        fixed(quality: 100) {
-          ...GatsbyImageSharpFixed
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
         }
       }
     }
     tapis: file(relativePath: { eq: "products/tapis.webp" }) {
       childImageSharp {
-        fixed(quality: 100) {
-          ...GatsbyImageSharpFixed
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
         }
       }
     }
