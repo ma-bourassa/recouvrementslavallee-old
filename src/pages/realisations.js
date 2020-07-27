079c070e2bf510db5a2bebe8b4a37b3396131560
Marc-Andre Bourassa <boumar04@gmail.com>
2020-07-26 21:43:24 -0400

    photo gallery

--- src/pages/realisations.js
+++ src/pages/realisations.js
@@ -1,12 +1,52 @@
 import React from "react";
-
+import { graphql } from "gatsby";
 import Layout from "../components/layout/layout";
+import PropTypes from "prop-types";
 import SEO from "../components/seo";
+import Gallery from "@browniebroke/gatsby-image-gallery";
+import "@browniebroke/gatsby-image-gallery/dist/style.css";
 
-export default function RealisationsPage() {
+export default function RealisationsPage({ data }) {
+  const images = data.allFile.edges.map(({ node }) => node.childImageSharp);
+  const lightboxOptions = {
+    imageLoadErrorMessage: "Impossible de charger cette image",
+    nextLabel: "Image suivante",
+    prevLabel: "Image précédente",
+    zoomInLabel: "Zoomer",
+    zoomOutLabel: "Dézoomer",
+    closeLabel: "Fermer",
+  };
+
   return (
     <Layout>
       <SEO keywords={["realisations", "modeles"]} title="Realisations" />
+      <div className="max-w-screen-lg mx-auto">
+        <Gallery images={images} lightboxOptions={lightboxOptions} />
+      </div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query ImagesForGallery {
+    allFile(filter: { sourceInstanceName: { eq: "realisations" } }) {
+      edges {
+        node {
+          childImageSharp {
+            thumb: fluid(maxWidth: 300, maxHeight: 300) {
+              ...GatsbyImageSharpFluid
+              ...GatsbyImageSharpFluidLimitPresentationSize
+            }
+            full: fluid(quality: 100, maxWidth: 1024) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+RealisationsPage.propTypes = {
+  data: PropTypes.object,
+};
