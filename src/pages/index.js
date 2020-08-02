7f21e6538363b8215c3f8d8fce2a89984877dfed
Marc-Andre Bourassa <boumar04@gmail.com>
2020-08-02 18:20:11 -0400

    produits

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,9 @@ export default function IndexPage({ data }) {
             <div>
               <h1 className="font-bold text-2xl lg:text-5xl">Vente et installation de plancher</h1>
               <hr className="hr" />
-              <h4 className="lg:max-w-3xl lg:mx-auto text-lg lg:text-2xl">Contactez nous pour une soumission</h4>
+              <h4 className="lg:max-w-3xl lg:mx-auto text-lg lg:text-2xl">
+                Contactez nous pour une soumission gratuite
+              </h4>
               <Link to="/contact/">
                 <button className="mt-6 btn btn-blue text-xl rounded-full">Soumission</button>
               </Link>
@@ -24,10 +26,10 @@ export default function IndexPage({ data }) {
         </section>
       </BackgroundImage>
 
-      <section className="container mx-auto px-6 py-6 lg:py-12 lg:max-w-5xl">
+      <section className="container mx-auto px-6 py-6 lg:py-12 lg:max-w-5xl leading-relaxed">
         <h3 className="text-xl lg:text-3xl font-semibold text-center">Vos experts en pose de plancher</h3>
         <hr className="border-blue2 border-t-2 mx-auto my-6 w-full" />
-        <div className="font-light text-center lg:text-lg ">
+        <div className="font-light text-center lg:text-lg">
           Chers clients, nous sommes ravis de constater votre intérêt pour notre entreprise familiale qui se spécialise
           dans la pose et la vente de revêtements de sols souples. Sachez que nous sommes une compagnie sérieuse et
           établie qui opère depuis 1985 sur la Rive Sud de Montréal.
@@ -35,7 +37,7 @@ export default function IndexPage({ data }) {
       </section>
 
       <section className="bg-gray-100 ">
-        <div className="container mx-auto flex flex-col lg:flex-row px-6 py-6 lg:py-12">
+        <div className="container mx-auto flex flex-col lg:flex-row px-6 py-6 lg:py-12 leading-relaxed">
           <div className="w-1/3 mx-12">
             <hr className="border-blue2 border-t-2 my-4 mx-12" />
             <h4 className="text-xl lg:text-2xl font-semibold text-center mx-12">
@@ -43,7 +45,7 @@ export default function IndexPage({ data }) {
             </h4>
             <hr className="border-blue2 border-t-2 my-4 mx-12" />
           </div>
-          <div className="w-1/3 font-light text-justify lg:text-base mx-12">
+          <div className="w-1/3 font-light text-justify lg:text-base mx-12 ">
             <p>
               Chez nous, il n&apos;y a pas de demie-mesure, car la qualité et la finition sont capitales. Notre service
               est clé en main et 100% adapté à vos souhaits!
@@ -64,6 +66,26 @@ export default function IndexPage({ data }) {
             <Link to="/contact/">
               <button className="mt-6 py-2 px-4 font-semibold inline-block shadow-md cursor-pointer text-lg border border-blue2 hover:bg-blue2 hover:text-white rounded-full focus:outline-none">
                 Contactez-nous
+              </button>
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container mx-auto flex flex-col lg:flex-row px-6 py-6 lg:py-12 leading-relaxed">
+          <div className="w-1/3 mx-12">
+            <h4 className="text-xl lg:text-2xl font-semibold text-center mx-12">PHOTO </h4>
+          </div>
+          <div className="w-2/3 font-light text-justify lg:text-base mx-12">
+            <h4 className="text-xl lg:text-2xl font-semibold mb-4">Nos spécialitées</h4>
+            <p className="font-light lg:text-lg">
+              Nous pouvons poser pratiquement tous les types de revêtements de sols. Nos spécialités sont toutefois les
+              lattes de vinyle, les tapis, les carreaux de tapis, le linoléum, les prélarts et les bois d’ingénieries.
+            </p>
+            <Link to="/produits/">
+              <button className="mt-6 py-2 px-4 font-semibold inline-block shadow-md cursor-pointer text-lg border border-blue2 hover:bg-blue2 hover:text-white rounded-full focus:outline-none">
+                Plus d&apos;informations
               </button>
             </Link>
           </div>
