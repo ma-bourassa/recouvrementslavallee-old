c096074b368f094462329c78873b4636c1180bb2
Marc-Andre Bourassa <boumar04@gmail.com>
2020-08-02 18:43:18 -0400

    style

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import SEO from "../components/seo";
 import { Link, graphql } from "gatsby";
 import PropTypes from "prop-types";
 import BackgroundImage from "gatsby-background-image";
+import Img from "gatsby-image";
+
 export default function IndexPage({ data }) {
   return (
     <Layout>
       <SEO title="Accueil" />
-      <BackgroundImage fluid={data.indexImage.childImageSharp.fluid}>
+      <BackgroundImage fluid={data.headerImage.childImageSharp.fluid}>
         <section className="py-8 lg:py-16 text-center text-white">
           <div className="bg-grey py-4 bg-opacity-50">
             <div>
@@ -36,16 +38,16 @@ export default function IndexPage({ data }) {
         </div>
       </section>
 
-      <section className="bg-gray-100 ">
-        <div className="container mx-auto flex flex-col lg:flex-row px-6 py-6 lg:py-12 leading-relaxed">
-          <div className="w-1/3 mx-12">
-            <hr className="border-blue2 border-t-2 my-4 mx-12" />
-            <h4 className="text-xl lg:text-2xl font-semibold text-center mx-12">
-              Pourquoi choisir notre entreprise pour vos travaux ?
+      <section className="bg-gray-200 ">
+        <div className="container mx-auto flex flex-col lg:flex-row px-6 py-6 lg:py-12 leading-relaxed items-center">
+          <div className="lg:w-1/3 lg:mx-12">
+            <hr className="border-blue2 border-t-2 my-4" />
+            <h4 className="text-xl lg:text-2xl font-semibold text-center">
+              Pourquoi choisir notre entreprise pour vos travaux&nbsp;?
             </h4>
-            <hr className="border-blue2 border-t-2 my-4 mx-12" />
+            <hr className="border-blue2 border-t-2 my-4" />
           </div>
-          <div className="w-1/3 font-light text-justify lg:text-base mx-12 ">
+          <div className="lg:w-1/3 font-light text-justify mx-12 ">
             <p>
               Chez nous, il n&apos;y a pas de demie-mesure, car la qualité et la finition sont capitales. Notre service
               est clé en main et 100% adapté à vos souhaits!
@@ -56,7 +58,7 @@ export default function IndexPage({ data }) {
               prix et d&apos;avoir la meilleure équipe de pose sur le marché.
             </p>
           </div>
-          <div className="w-1/3 font-light text-justify lg:text-base mx-12">
+          <div className="lg:w-1/3 font-light text-justify mx-12">
             <p>
               Comme toute compagnie professionnelle incorporée, nous avons une assurance afin de vous protéger de tout
               imprévus liés à notre passage ou à toutes erreurs de construction.
@@ -74,17 +76,17 @@ export default function IndexPage({ data }) {
 
       <section>
         <div className="container mx-auto flex flex-col lg:flex-row px-6 py-6 lg:py-12 leading-relaxed">
-          <div className="w-1/3 mx-12">
-            <h4 className="text-xl lg:text-2xl font-semibold text-center mx-12">PHOTO </h4>
+          <div className="hidden lg:block w-1/3">
+            <Img fluid={data.productImage.childImageSharp.fluid}></Img>
           </div>
-          <div className="w-2/3 font-light text-justify lg:text-base mx-12">
-            <h4 className="text-xl lg:text-2xl font-semibold mb-4">Nos spécialitées</h4>
-            <p className="font-light lg:text-lg">
+          <div className="w-2/3 font-light mx-12 ">
+            <h4 className="text-2xl text-center lg:text-left font-semibold mb-4">Nos spécialitées</h4>
+            <p className="text-justify lg:text-left font-light">
               Nous pouvons poser pratiquement tous les types de revêtements de sols. Nos spécialités sont toutefois les
               lattes de vinyle, les tapis, les carreaux de tapis, le linoléum, les prélarts et les bois d’ingénieries.
             </p>
             <Link to="/produits/">
-              <button className="mt-6 py-2 px-4 font-semibold inline-block shadow-md cursor-pointer text-lg border border-blue2 hover:bg-blue2 hover:text-white rounded-full focus:outline-none">
+              <button className="mt-6 py-2 px-4 font-semibold inline-block shadow-md cursor-pointer border border-blue2 hover:bg-blue2 hover:text-white rounded-full focus:outline-none">
                 Plus d&apos;informations
               </button>
             </Link>
@@ -97,7 +99,14 @@ export default function IndexPage({ data }) {
 
 export const query = graphql`
   query {
-    indexImage: file(relativePath: { eq: "floor4.jpg" }) {
+    headerImage: file(relativePath: { eq: "floor4.jpg" }) {
+      childImageSharp {
+        fluid(quality: 90) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    productImage: file(relativePath: { eq: "floor.jpg" }) {
       childImageSharp {
         fluid(quality: 90) {
           ...GatsbyImageSharpFluid
