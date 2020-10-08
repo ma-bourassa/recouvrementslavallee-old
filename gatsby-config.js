8a1e163b1dc624370c22d182a81054e07d9a1269
Marc-Andre Bourassa <boumar04@gmail.com>
2020-10-07 21:39:43 -0400

    netlifycms

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     "gatsby-plugin-robots-txt",
     `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-netlify`,
+    `gatsby-plugin-netlify-cms`,
     `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
