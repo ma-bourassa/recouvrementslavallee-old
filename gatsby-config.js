29040ff6f8f002f689697d11d7aede8318a90c68
Marc-Andre Bourassa <boumar04@gmail.com>
2020-10-29 16:51:55 -0400

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
             resolve: "gatsby-remark-images",
             options: {
               maxWidth: 1024,
-              linkImagesToOriginal: false,
+              withWebp: true,
             },
           },
         ],
