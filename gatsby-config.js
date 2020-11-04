3805865f61021393739c3aa7f840dd2a7d2ff837
Marc-Andre Bourassa <boumar04@gmail.com>
2020-11-03 19:01:27 -0500

    data source

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,20 +49,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `data`,
-        path: `${__dirname}/src/data`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: "products",
-        path: `${__dirname}/src/images/products`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: "images",
         path: `${__dirname}/src/images`,
       },
